a509a56985df0f28263f74a5accb76a896dee3f5
Marina Vasovic <Marina.Vasovic@udg.edu.me>
2018-04-12 08:57:54 +0200

    zadatak4

--- task4.py
+++ task4.py
@@ -14,23 +14,43 @@
 ===================================================
 """
 
-# Write your function here
-
-
-
-
-
 def convert_2_upper(recenica):
-    g = ""
-    for ch in recenica:
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            x = ord(ch) - 32
-            y = chr(x)
-            g = g + y
-    return g
-def main():
-    recenica = input('Enter here: ')
-    print(convert_2_upper(recenica))
 
+    if not isinstance(recenica,str): # provjeravamo da li je unijet string
+        print("Pogresan unos!")
+
+    nova_recenica = '' # data recenica ispisana velikim slovima,koju treba da dobijemo,je na pocetku prazna
+
+    for karakter in recenica: # prolazimo kroz svaki karakter u recenici
+        broj_chr = ord(karakter) # svaki karakter pretvaramo u ASCII kod(odgovarajuci broj)
+        if broj_chr > 96 and broj_chr < 123: # mala slova u ASCII kodu pocinju sa 97 i zavrsavaju sa 122,a velika su od 65-90
+                                # pa svako slovo date recenice ciji odgovarajuci broj u ASCII kodu odgovara malom slovu
+                                # smanjicemo za 32 i dobiti njemu odgovarajuce veliko slovo
+
+
+
+            broj_velikog_slova = broj_chr - 32
+            karakter = chr (broj_velikog_slova) # vratimo broj u odgovarajuce slovo
+
+
+        nova_recenica += karakter # karakter po karakter dodajemo u novu recenicu
+
+    return nova_recenica # fja vraca novu recenicu,koja je sada ispisana velikim slovima
+
+def main():
+
+    recenica= "MaRiNa VaSoViC"
+    print("Data recenica napisana velikim slovima : ", convert_2_upper(recenics))
+
+
+
+
+    pass
 
 main()
+
+
+
+
+
+
